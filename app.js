e3b3adc99d24bb25a54cf18d1f920b4bb672e68a
Kayleigh <57508868+gordokay@users.noreply.github.com>
2023-01-13 12:45:43 -0500

    Fix IntersectionObserver

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const header = document.querySelector('header');
 const section = document.querySelector('section');
 const navList = document.querySelector('nav > ul');
 
-const observer = new IntersectionObserver(toggleActiveNavLink);
+const observer = new IntersectionObserver(toggleActiveNavLink, {threshold: 0.5});
 
 function toggleActiveNavLink(entries) {
   entries.forEach(entry => {
     const navLink = navList.querySelector(`li > a[href='#${entry.target.id}']`);
-    if(entry.intersectionRatio < 0.5 && entry.isIntersecting) {
+    if(entry.isIntersecting) {
       navLink.classList.add('active');
     } else {
       navLink.classList.remove('active');
